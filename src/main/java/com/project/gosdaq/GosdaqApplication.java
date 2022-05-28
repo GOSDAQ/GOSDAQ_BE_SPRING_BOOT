c85a42867b3ee30e022a820e15763895812b2eb4
gangslee <grandnet1225@gmail.com>
2022-05-28 18:08:23 +0900

    Init Project

---
+++ src/main/java/com/project/gosdaq/GosdaqApplication.java
@@ -0,0 +1,13 @@
+package com.project.gosdaq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GosdaqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GosdaqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/gosdaq/GosdaqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.gosdaq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GosdaqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
