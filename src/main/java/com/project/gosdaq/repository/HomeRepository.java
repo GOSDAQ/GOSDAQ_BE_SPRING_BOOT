f525e316d6450003f9bf2d022f2c6de35e66ca5a
gangslee <grandnet1225@gmail.com>
2022-06-01 20:48:57 +0900

    [관심종목] API 개발 완료(현재가, 등락률)

--- src/main/java/com/project/gosdaq/repository/HomeRepository.java
+++ src/main/java/com/project/gosdaq/repository/HomeRepository.java
@@ -2,8 +2,6 @@ package com.project.gosdaq.repository;
 
 import com.project.gosdaq.dto.home.HomeResponseDTO;
 
-import java.util.List;
-
 public interface HomeRepository {
     HomeResponseDTO getStockData(String tickers);
 }
